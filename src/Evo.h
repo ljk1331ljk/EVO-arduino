4aaac3eeab734a494b47451a425d6d1421162950
ljk1331ljk <limitless1331@gmail.com>
2025-04-10 23:55:15 +0800

    removed EvoImu, EvoTrace included EvoBNO055 and EvoHC05

--- src/Evo.h
+++ src/Evo.h
@@ -5,17 +5,17 @@
 
 #include "EVOX1.h"
 
+#include "helper/EvoHC05.h"
 
 #include "motors/EvoMotor.h"
 #include "motors/EvoServo.h"
 
 #include "robotics/EvoMotorPair.h"
-#include "robotics/EvoTrace.h"
 
 #include "sensors/EV3ColorSensor.h"
 #include "sensors/EvoTCS34725.h"
 #include "sensors/EvoVL53L0X.h"
+#include "sensors/EvoBNO055.h"
 #include "sensors/EvoHuskyLens.h"
-#include "sensors/EvoIMU.h"
 
 #endif
