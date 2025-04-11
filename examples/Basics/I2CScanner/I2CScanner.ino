594a9ce70cf4137ed974b92d3b2abbcab55c0a07
ljk1331ljk <limitless1331@gmail.com>
2025-04-11 10:34:51 +0800

    clean up examples

--- examples/Basics/I2CScanner/I2CScanner.ino
+++ examples/Basics/I2CScanner/I2CScanner.ino
@@ -2,10 +2,11 @@
 
 EVOX1 evo;
 
-void setup() {
+void setup()
+{
   evo.begin();
   Serial.begin(115200);
-  while (!Serial)  //Waits for serial to be connected
+  while (!Serial) // Waits for serial to be connected
     ;
   Serial.println("I2C Scanner");
   delay(1000);
@@ -14,11 +15,14 @@ void setup() {
   int maxAddresses = 5;
   int numAddress = 0;
   uint8_t i2CAddress[maxAddresses];
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 8; i++)
+  {
     numAddress = evo.scanI2CChannel((I2CChannel)i, i2CAddress, maxAddresses);
-    if (numAddress != 0) {
-      Serial.printf("%d addresse(s) found on I2C%d:", numAddress, i+1);
-      for (int j = 0; j < numAddress; j++) {
+    if (numAddress != 0)
+    {
+      Serial.printf("%d addresse(s) found on I2C%d:", numAddress, i + 1);
+      for (int j = 0; j < numAddress; j++)
+      {
         Serial.printf(" 0x%x", i2CAddress[j]);
       }
       Serial.println();
@@ -26,6 +30,7 @@ void setup() {
   }
 }
 
-void loop() {
+void loop()
+{
   delay(1000);
 }
