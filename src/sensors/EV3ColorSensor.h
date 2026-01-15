301b47c164381798002db9d03c08db4a3249c134
ljk1331ljk <limitless1331@gmail.com>
2026-01-15 12:27:59 +0800

    added documentation for ev3colorsensor

--- src/sensors/EV3ColorSensor.h
+++ src/sensors/EV3ColorSensor.h
@@ -27,18 +27,65 @@ enum struct EV3ColorSensorMode : uint8_t
     COL_REFLECT = 0
 };
 
+/**
+ * @class EV3ColorSensor
+ * @brief Class to interact with the EV3 Color Sensor.
+ */
 class EV3ColorSensor
 {
 public:
-    SensorPort _port;
+    /**
+     * @brief Constructs an EV3ColorSensor object.
+     * @param port The sensor port the color sensor is connected to.
+     */
     EV3ColorSensor(SensorPort port);
+
+    /**
+     * @brief Initializes the EV3 Color Sensor.
+     * @return True if the EV3 Color Sensor was successfully initialized, false otherwise.
+     */
     bool begin();
+
+    /**
+     * @brief Sets the operating mode of the EV3 Color Sensor.
+     * @param mode The operating mode to set
+     * COL_CAL
+     * RGB_RAW
+     * REF_RAW
+     * COL_COLOR
+     * COL_AMBIENT
+     * COL_REFLECT
+     */
     void setMode(EV3ColorSensorMode mode);
+
+    /**
+     * @brief Retrieves the reflected light sensor value.
+     * @return reflected light sensor value.
+     */
     int getReflection();
+
+    /**
+     * @brief Retrieves the ambient light sensor value.
+     * @return ambient light sensor value.
+     */
     int getAmbient();
+
+    /**
+     * @brief Retrieves the detected color.
+     * @return detected color as EV3ColorSensorColor enum.
+     * NONE = 0
+     * COLOR_BLACK = 1
+     * COLOR_BLUE = 2
+     * COLOR_GREEN = 3
+     * COLOR_YELLOW = 4
+     * COLOR_RED = 5
+     * COLOR_WHITE = 6
+     *COLOR_BROWN = 7
+     */
     int getColor();
 
 private:
+    SensorPort _port;
     EvoSensorPort _EV3port;
     EV3ColorSensorMode _mode;
 
